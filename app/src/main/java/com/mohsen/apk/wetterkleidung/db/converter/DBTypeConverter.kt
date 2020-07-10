87c6905e094de68bd632a7c2f0d1ae34412ba29c
mohsen <mohsen.apk@gmail.com>
2020-07-10 16:41:44 +0200

    add forecastWeather models

--- app/src/main/java/com/mohsen/apk/wetterkleidung/db/converter/DBTypeConverter.kt
+++ app/src/main/java/com/mohsen/apk/wetterkleidung/db/converter/DBTypeConverter.kt
@@ -3,20 +3,34 @@ package com.mohsen.apk.wetterkleidung.db.converter
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import com.mohsen.apk.wetterkleidung.model.CurrentWeatherTitle
+import com.mohsen.apk.wetterkleidung.model.ForecastWeatherDetail
+import com.mohsen.apk.wetterkleidung.model.WeatherTitle
 
 class DBTypeConverter {
 
     @TypeConverter
-    fun fromCurrentWeatherTitleList(list: List<CurrentWeatherTitle>): String {
-        val type = object : TypeToken<List<CurrentWeatherTitle>>() {}.type
+    fun fromCurrentWeatherTitleList(list: List<WeatherTitle>): String {
+        val type = object : TypeToken<List<WeatherTitle>>() {}.type
         return Gson().toJson(list, type)
     }
 
     @TypeConverter
-    fun toCurrentWeatherTitleList(jsonStr: String): List<CurrentWeatherTitle> {
-        val type = object : TypeToken<List<CurrentWeatherTitle>>() {}.type
+    fun toCurrentWeatherTitleList(jsonStr: String): List<WeatherTitle> {
+        val type = object : TypeToken<List<WeatherTitle>>() {}.type
         return Gson().fromJson(jsonStr, type)
     }
+
+    @TypeConverter
+    fun fromForecastWeatherDetailList(list: List<ForecastWeatherDetail>): String {
+        val type = object : TypeToken<List<WeatherTitle>>() {}.type
+        return Gson().toJson(list, type)
+    }
+
+    @TypeConverter
+    fun toForecastWeatherDetailList(jsonStr: String): List<ForecastWeatherDetail> {
+        val type = object : TypeToken<List<WeatherTitle>>() {}.type
+        return Gson().fromJson(jsonStr, type)
+    }
+
 
 }
